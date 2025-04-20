ee35ac0c28a07c73202a0a4546cf85704daf8cb8
Raj <rajbaidyanath76@gmail.com>
2025-04-20 20:47:27 +0530

    done

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = mongoose.Schema(
     {
       name: { type: "String", required: true },
-      email: { type: "String", unique: true, required: true },
+      email: { type: "String", unique: true, required: true, unique: true },
       password: { type: "String", required: true },
       pic: {
         type: "String",
