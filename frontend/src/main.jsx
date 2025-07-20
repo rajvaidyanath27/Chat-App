ef9c8f80b0d11d896b8cb56fcdd4fc24e9580bb7
Raj <rajbaidyanath76@gmail.com>
2025-07-21 02:35:58 +0530

    one

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,10 @@
-import { Provider } from "@/components/ui/provider"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom';
-import { ChakraProvider } from "@chakra-ui/react"
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-     <Provider>
-      <App />
-    </Provider>
-    </BrowserRouter>
-  </StrictMode>
+    <App />
+  </StrictMode>,
 )
