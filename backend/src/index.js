fd0ea731dbdae8557438ecb71bd8b4c52ecd4ea0
Raj <rajbaidyanath76@gmail.com>
2025-07-21 15:33:24 +0530

    Fix: updated index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,8 @@
+import cookieParser from "cookie-parser";
 import express from "express";
 const app = express();
 import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
+
 import cors from "cors";
 
 import { fileURLToPath } from "url";
@@ -33,13 +34,13 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+//   });
+// }
 
 
 
